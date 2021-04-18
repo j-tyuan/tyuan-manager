76772a24eb6c0e887e113aaa1280988412a1c951
guiqijiang <jiangguiqi@fintell.com.cn>
2021-04-18 20:12:34 +0800

    迁移通用resultdata类

--- tyuan-common/src/main/java/org/tyuan/common/ResultData.java
+++ tyuan-common/src/main/java/org/tyuan/common/ResultData.java
@@ -18,9 +18,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.tyuan.service.system.model;
+package org.tyuan.common;
 
 import com.alibaba.fastjson.JSONObject;
+import org.tyuan.common.config.ErrorCodeConsts;
 
 import java.io.Serializable;
 
